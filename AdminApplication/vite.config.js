1ea4e77ac22d7fc3dd3da83b8387b37ed577f398
Pershin <202309635@post.au.dk>
2025-11-02 20:49:31 +0100

    fix nogen ting

--- AdminApplication/vite.config.js
+++ AdminApplication/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AdminApplication/'
+  base: '/AdminApplication/',
+  build: {
+    sourcemap: false,          // 👈 disables eval-based code that CSP blocks
+  },
 })
