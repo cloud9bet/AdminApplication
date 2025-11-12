76d81a1a5432396cc7492223cd6a35722b1e78c2
Pershin <202309635@post.au.dk>
2025-11-12 12:55:58 +0100

    opdateret mock data

--- AdminApplication/src/mock/mockTransactions.js
+++ AdminApplication/src/mock/mockTransactions.js
@@ -1,25 +1,25 @@
 export const mockTransactions = [
   // SlotMachine
-  { transactionId: 3, date: "2025-11-05", amount: 1500, gameName: "SlotMachine" },
-  { transactionId: 4, date: "2025-11-06", amount: 4200, gameName: "SlotMachine" },
-  { transactionId: 5, date: "2025-11-07", amount: 2800, gameName: "SlotMachine" },
-  { transactionId: 6, date: "2025-11-08", amount: 3300, gameName: "SlotMachine" },
-  { transactionId: 7, date: "2025-11-09", amount: 4800, gameName: "SlotMachine" },
-  { transactionId: 8, date: "2025-11-10", amount: 5200, gameName: "SlotMachine" },
+  { transactionId: 3, date: "2025-11-07", amount: 1500, gameName: "SlotMachine" },
+  { transactionId: 4, date: "2025-11-08", amount: 4200, gameName: "SlotMachine" },
+  { transactionId: 5, date: "2025-11-09", amount: 2800, gameName: "SlotMachine" },
+  { transactionId: 6, date: "2025-11-10", amount: 3300, gameName: "SlotMachine" },
+  { transactionId: 7, date: "2025-11-11", amount: 4800, gameName: "SlotMachine" },
+  { transactionId: 8, date: "2025-11-12", amount: 5200, gameName: "SlotMachine" },
 
   // Crash
-  { transactionId: 11, date: "2025-11-05", amount: 3700, gameName: "Crash" },
-  { transactionId: 12, date: "2025-11-06", amount: 4500, gameName: "Crash" },
-  { transactionId: 13, date: "2025-11-07", amount: 6400, gameName: "Crash" },
-  { transactionId: 14, date: "2025-11-08", amount: 3900, gameName: "Crash" },
-  { transactionId: 15, date: "2025-11-09", amount: 7234, gameName: "Crash" },
-  { transactionId: 16, date: "2025-11-10", amount: 4100, gameName: "Crash" },
+  { transactionId: 11, date: "2025-11-07", amount: 3700, gameName: "Crash" },
+  { transactionId: 12, date: "2025-11-08", amount: 4500, gameName: "Crash" },
+  { transactionId: 13, date: "2025-11-09", amount: 6400, gameName: "Crash" },
+  { transactionId: 14, date: "2025-11-10", amount: 3900, gameName: "Crash" },
+  { transactionId: 15, date: "2025-11-11", amount: 7234, gameName: "Crash" },
+  { transactionId: 16, date: "2025-11-12", amount: 4100, gameName: "Crash" },
 
   // Coinflip
-  { transactionId: 19, date: "2025-11-05", amount: 2000, gameName: "Coinflip" },
-  { transactionId: 20, date: "2025-11-06", amount: 2374, gameName: "Coinflip" },
-  { transactionId: 21, date: "2025-11-07", amount: 4000, gameName: "Coinflip" },
-  { transactionId: 22, date: "2025-11-08", amount: 1733, gameName: "Coinflip" },
-  { transactionId: 23, date: "2025-11-09", amount: 3900, gameName: "Coinflip" },
-  { transactionId: 24, date: "2025-11-10", amount: 879, gameName: "Coinflip" },
+  { transactionId: 19, date: "2025-11-07", amount: 2000, gameName: "Coinflip" },
+  { transactionId: 20, date: "2025-11-08", amount: 2374, gameName: "Coinflip" },
+  { transactionId: 21, date: "2025-11-09", amount: 4000, gameName: "Coinflip" },
+  { transactionId: 22, date: "2025-11-10", amount: 1733, gameName: "Coinflip" },
+  { transactionId: 23, date: "2025-11-11", amount: 3900, gameName: "Coinflip" },
+  { transactionId: 24, date: "2025-11-12", amount: 879, gameName: "Coinflip" },
 ];
