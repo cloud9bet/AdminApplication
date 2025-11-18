320e69818964c15e7cb6383567d8fe8376a34ed6
andreasfrost1004 <dk.andreasfrost@gmail.com>
2025-11-18 09:50:46 +0100

    added userApi

---
+++ AdminApplication/src/services/userApi.js
@@ -0,0 +1,27 @@
+import { Api } from "../apiService";
+
+//alt logges lige nu men skal ændres til at return stuff når de er testet
+
+export async function GetAllUserDepositAsync() {
+    try {
+        const response = await Api.get(`/deposit`);
+        console.log(response.data);
+        return response.data;
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+export async function GetAllUserTransactionAsync() {
+    try {
+        const response = await Api.get(`/User/transaction`); // skal ændres  med (User/transaction)
+        console.log(response.data);
+        return response.data;
+
+    } catch (error) {
+        console.error(error.message);
+        return false;
+    }
+
+}
